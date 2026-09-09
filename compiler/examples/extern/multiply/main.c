ec265cc523a77d93ea8e2e4436ce41d3a1e71560
Ramiro Lugo <ramiro.lugo@mi.unc.edu.ar>
2026-09-08 22:53:11 -0300

    feat: add examples

---
+++ compiler/examples/extern/multiply/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdint.h>
+
+uint64_t add(uint64_t x, uint64_t y) {
+    printf("USED IN JASMIN: add(%lu, %lu)\n", x, y);
+    return x + y;
+}
+
+extern uint64_t run_multiply(uint64_t a, uint64_t b);
+
+int main(void) {
+    uint64_t result;
+
+    result = run_multiply(5, 3);
+    printf("RESULT        : run_multiply(5, 3) = %lu \n", result);
+
+    printf("\n");
+
+
+    result = run_multiply(100, 200);
+    printf("RESULT        : run_multiply(100,200) = %lu \n", result);
+
+    return 0;
+}
